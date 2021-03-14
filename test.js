967b768e11e6dca79fc4bd940942ecc8f9c14e5d
yxiuzhu <y_xiuzhu@163.com>
2021-03-14 22:29:58 +0800

    路由懒加载

--- test.js
+++
@@ -1,8 +0,0 @@
-var obj = {
-  name: '彭湖湾',
-  job: '学生'
-}
-var copyObj = Object.assign({}, obj);
-copyObj.name = '我才不叫彭湖湾呢！ 哼  (。・`ω´・)';
-console.log(obj);   // {name: "彭湖湾", job: "学生"}
-console.log(copyObj);  // {name: "我才不叫彭湖湾呢！ 哼  (。・`ω´・)", job: "学生"}
